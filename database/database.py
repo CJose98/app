9441db034d7bce9ef5d18d79d772a34d1174a06c
Tula Miguel <matula77@yahoo.com.ar>
2023-09-06 11:00:00 -0300

    cambios en database

--- database/database.py
+++ database/database.py
@@ -110,3 +110,38 @@ class DatabaseConnection_2:
         if cls._connection is not None:
             cls._connection.close()
             cls._connection = None
+class DataBaseConnection1:
+    _connection=None
+    @classmethod
+    def get_connection(cls):
+        if cls._connection is None:
+            cls._connection=mysql.connector.connect(
+                host="127.0.0.1",
+                user="root",
+                port="3306",
+                password="123456",
+                database="sakila",
+                )
+            return  cls._connection
+    @classmethod
+    def execute_query(cls,query,params=None):
+        cursor=cls.get_connection().cursor()
+        cursor.execute(query,params)
+        cls._connection.commit()
+        return cursor
+    @classmethod
+    def fetch_one(cls,query,params=None):
+        cursor=cls.get_connection().cursor()
+        cursor.execute(query,params)
+        return cursor.fetchone()
+    @classmethod
+    def fetch_all(cls,query,params=None):
+            
+        cursor=cls.get_connection().cursor()
+        cursor.execute(query,params)
+        return cursor.fetchall()
+    @classmethod
+    def close_connection(cls):
+        if cls._connection is not None:
+            cls._connection.close()
+            cls._connection =None
