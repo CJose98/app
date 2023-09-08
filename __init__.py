290243139c995bafd2b987e7670bf972c7356a36
Tula Miguel <matula77@yahoo.com.ar>
2023-09-08 11:22:01 -0300

    cambios en init

--- __init__.py
+++ __init__.py
@@ -9,5 +9,29 @@ def inicializar_app():
         app.config.from_object(Config)
         app.register_blueprint(actor_bp)
 
-                
+        @app.route('/')
+        def init():
+                pass#return render_template ('login.html')
+        @app.route('/NuevaSala', methods=['post'])
+        def sala():
+            return crear_sala()
+        
+#                    insert into user_1.salas (Nombre_sala) values('Literatura Fantastica','Locura Cinematografica','Relatos de Viajes',
+# 'Salud y Bienestar','Central de Juegos','Fanaticos del Deporte','Caos Musical'); 
+
+#crear usuario 
+# insert into user_1.usuario (Apellido,Nombre,Correo,Contraseña,Nombre_usuario,Fecha_nac)
+# values(%s,%s,%s,%s,%s,%s),
+# # ('Farfan','Celeste','CElestef@hotamil.com','456','cele21','2-2-2005'),
+# ('Colque','Jose','cjose@gmail.com','741258','CJose98','3/3/2002');
+
+# SELECT Nombre_sala FROM user_1.salas where id_Salas=5;
+# select Apellido,Nombre,Correo,Nombre_sala from usuario 
+# inner join salas
+# on usuario.user_id=salas.id_Salas
+# where id_user=3;
+
+# delete from user_1.salas where id_Salas=8;
+#  UPDATE salas SET Nombre_sala= 'Prueba no boorar' WHERE id_Salas=9;
+
         return app
