914d448260cf8d55303c0bc0869feccc19f45421
matula77 <matula77@yahoo.com.ar>
2023-09-20 10:00:41 -0300

    creacion servidro

--- models/auth/sala_model.py
+++ models/auth/sala_model.py
@@ -8,18 +8,19 @@ class Sala:
     def __init__(self, **kwargs):
         self.id_servidor = kwargs.get('id_servidor')
         self.nombre_servidor = kwargs.get('nombre_servidor')
+        self.propietario_id=kwargs.get('nombre_propietario')
 
     def serialize(self):
         return  {
             "id_servidor": self.id_servidor,
-            "nombre_servidor": self.nombre_servidor
-            
+            "nombre_servidor": self.nombre_servidor,
+            "propietario_id":self.propietario_id
+
             #"status": UserStatusModel.get(UserStatusModel(status_id = self.status_id)).serialize(),
             #"role": UserRoleModel.get(UserRoleModel(role_id = self.role_id)).serialize()
         }
 
     @classmethod
-<<<<<<< HEAD
     def get(cls, sala):
         query = """SELECT salas.id_sala, salas.nombre_sala
                     FROM user_1.salas INNER JOIN user_1.usuario
@@ -39,32 +40,18 @@ class Sala:
         # append.sala{'nombresala':result[1],
         #             'id_sala':result[0]}
         return None
-=======
-    def get(cls, user):
-        query = """SELECT servidores.id_servidor, servidores.nombre_servidor
-                FROM Discor.servidores INNER JOIN Discor.usuarios
-                ON servidores.propietario_id = usuarios.id_usuario
-                WHERE usuarios.correo = %(correo)s""" #  ver q sea una lista
-                            
-        params = user.__dict__   #los datos del usuario logeado   #{"correo": user.correo} 
-        result = DatabaseConnection.fetch_all(query, params=params) #fetch_all (DEBE OBTENER UNA LISTA) 
+    ##----------------------------------------------------------##
+    ##creo las funcion crear 
+    @classmethod
+    def create_sala(cls):
+        sql = """INSERT INTO discord.servidores(nombre_servidor,
+        propietario_id)
+        values(%s,%s);"""
+        params = (Sala.nombre_servidor,Sala.propietario_id)
+        DatabaseConnection.execute_query(sql, params)
+        return None
     
-        if result:
-            if len(result) == 1:
-                # Si solo hay un resultado, devuélvelo como un solo objeto
-                return cls(
-                    id_servidor=result[0][0],
-                    nombre_servidor=result[0][1]
-                )
-            else:
-                # Si hay múltiples resultados, devuélvelos como una lista de objetos
-                return [cls(
-                    id_servidor=row[0],
-                    nombre_servidor=row[1]
-                ) for row in result]
-
-        return [] #None
->>>>>>> 87ba1b1e3f84b9f84c7a8ca1a7e21178987f46b5
+    
 
 
 
