0e1ae4975fcde5630d0e9d4a575a765b47f9010f
CJose98 <colquejose53@gmail.com>
2023-09-20 14:19:06 -0300

    modificacion de sala

--- models/auth/sala_model.py
+++ models/auth/sala_model.py
@@ -21,25 +21,30 @@ class Sala:
         }
 
     @classmethod
-    def get(cls, sala):
-        query = """SELECT salas.id_sala, salas.nombre_sala
-                    FROM user_1.salas INNER JOIN user_1.usuario
-                    ON salas.id_sala = usuario.id_user
-                    WHERE usuario.correo = %(correo)s"""
-        
-        params = sala.__dict__
-        result = DatabaseConnection.fetch_one(query, params=params) 
-        #fetch_all (DEBE OBTENER UNA LISTA O NO )
-        if result is not None:
-            return cls(
-                id_servidor = result[0],
-                nombre_servidor = result[1]
-            )
-        #fijate jose si no te falto for result in results
-        # sala=[]
-        # append.sala{'nombresala':result[1],
-        #             'id_sala':result[0]}
-        return None
+    def get(cls, user):
+        query = """SELECT servidores.id_servidor, servidores.nombre_servidor
+                FROM Discor.servidores INNER JOIN Discor.usuarios
+                ON servidores.propietario_id = usuarios.id_usuario
+                WHERE usuarios.correo = %(correo)s""" #  ver q sea una lista
+                            
+        params = user.__dict__   #los datos del usuario logeado   #{"correo": user.correo} 
+        result = DatabaseConnection.fetch_all(query, params=params) #fetch_all (DEBE OBTENER UNA LISTA) 
+    
+        if result:
+            if len(result) == 1:
+                # Si solo hay un resultado, devuélvelo como un solo objeto
+                return cls(
+                    id_servidor=result[0][0],
+                    nombre_servidor=result[0][1]
+                )
+            else:
+                # Si hay múltiples resultados, devuélvelos como una lista de objetos
+                return [cls(
+                    id_servidor=row[0],
+                    nombre_servidor=row[1]
+                ) for row in result]
+
+        return [] #None
     ##----------------------------------------------------------##
     ##creo las funcion crear 
     @classmethod
