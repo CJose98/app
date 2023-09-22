ea85db7e43f8805ac2a084c76c91ed5852767e2b
matula77 <matula77@yahoo.com.ar>
2023-09-22 10:00:07 -0300

    model msg userservi

---
+++ models/auth/user_servi_model.py
@@ -0,0 +1,9 @@
+from ...database import DatabaseConnection
+#from .user_role_model import UserRoleModel
+#from .user_status_model import UserStatusModel
+from flask import json, request
+
+class User_servi:
+    def __init__(self,**kwargs):
+        
+        
