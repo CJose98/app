611868d84623e5bfb3cc74e2fedebccef4635a08
matula77 <matula77@yahoo.com.ar>
2023-09-22 10:13:18 -0300

    subo todos los cambios

--- controllers/canal_controller.py
+++ controllers/canal_controller.py
@@ -3,9 +3,5 @@ from ..models.auth.user_model import User
 from ..models.auth.sala_model import Sala
 from flask import request, session, jsonify,render_template
 
-class User_servi_Controller:
-    @classmethod
-    def crear_servi():
-        return render_template():
-        pass
-   
+class CanalController:
+    
